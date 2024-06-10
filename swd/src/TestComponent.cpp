3b28e5bcffa2c28fa4db6989ecedac8575853b20
swd1986 <swd1986@yandex.ru>
2024-06-10 13:24:45 +0500

    refactor producer

--- swd/src/TestComponent.cpp
+++ swd/src/TestComponent.cpp
@@ -3,9 +3,11 @@
 #include <signal.h>
 #include <string.h>
 #include <string>
-//#include <librdkafka/rdkafka.h>
+#include <librdkafka/rdkafka.h>
 #include <iostream>
 #include <ctime>
+#include <locale>
+#include <codecvt>
 
 static volatile sig_atomic_t run = 1;
 
@@ -66,103 +68,123 @@ std::u16string TestComponent::getVersion()
 	return text + MB2WCHAR("MDM -> Linux Kafka Native версия: " + this->version());
 }
 
+/**
+ * @brief Signal termination of program
+ */
+static void stop(int sig)
+{
+	run = 0;
+	// fclose(stdin); /* abort fgets() */
+}
+
+static void
+dr_msg_cb(rd_kafka_t *rk, const rd_kafka_message_t *rkmessage, void *opaque)
+{
+	// if (rkmessage->err)
+	// fprintf(stderr, "%% Message delivery failed: %s\n",
+	//		rd_kafka_err2str(rkmessage->err));
+	// else
+	// fprintf(stderr,
+	//		"%% Message delivered (%zd bytes, "
+	//		"partition %" PRId32 ")\n",
+	//		rkmessage->len, rkmessage->partition);
+
+	/* The rkmessage is destroyed automatically by librdkafka */
+}
+
+std::string u16string_to_string(const std::u16string& u16str) {
+    std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> converter;
+    return converter.to_bytes(u16str);
+}
+
 std::u16string TestComponent::SendProducer(const std::u16string &p_text1, const std::u16string &p_text2)
 {
-	//rd_kafka_t *rk;		   /* Producer instance handle */
-	//rd_kafka_conf_t *conf; /* Temporary configuration object */
-	//char errstr[512];	   /* librdkafka API error reporting buffer */
-	//char buf[512];		   /* Message value temporary buffer */
-	//const char *brokers;   /* Argument: broker list */
-	//const char *topic;	   /* Argument: topic to produce to */
+	rd_kafka_t *rk;		   /* Producer instance handle */
+	rd_kafka_conf_t *conf; /* Temporary configuration object */
+	char errstr[512];	   /* librdkafka API error reporting buffer */
+	char buf[512];		   /* Message value temporary buffer */
+	const char *brokers;   /* Argument: broker list */
+	const char *topic;	   /* Argument: topic to produce to */
 
-	//brokers = "DC1TMSGBRKR01:9092,DC2TMSGBRKR01:9092,DC3TMSGBRKR01:9092";
-	//topic = "test";
+	brokers = "DC1TMSGBRKR01:9092,DC2TMSGBRKR01:9092,DC3TMSGBRKR01:9092";
+	topic = "test";
 
-	//conf = rd_kafka_conf_new();
+	conf = rd_kafka_conf_new();
 
-	// if (rd_kafka_conf_set(conf, "bootstrap.servers", brokers, errstr,
-	// 					  sizeof(errstr)) != RD_KAFKA_CONF_OK)
-	// {
-	// 	fprintf(stderr, "%s\n", errstr);
-	// 	return text + MB2WCHAR("error bootstrap.servers");
-	// }
+	if (rd_kafka_conf_set(conf, "bootstrap.servers", brokers, errstr,
+						  sizeof(errstr)) != RD_KAFKA_CONF_OK)
+	{
+		return text + MB2WCHAR("error bootstrap.servers");
+	}
 
-	// if (rd_kafka_conf_set(conf, "security.protocol", "SASL_PLAINTEXT",
-	// 					  errstr, sizeof(errstr)) != RD_KAFKA_CONF_OK)
-	// {
-	// 	return text + MB2WCHAR("error security.protocol");
-	// }
+	if (rd_kafka_conf_set(conf, "security.protocol", "SASL_PLAINTEXT",
+						  errstr, sizeof(errstr)) != RD_KAFKA_CONF_OK)
+	{
+		return text + MB2WCHAR("error security.protocol");
+	}
 
-	// if (rd_kafka_conf_set(conf, "sasl.mechanism", "PLAIN",
-	// 					  errstr, sizeof(errstr)) != RD_KAFKA_CONF_OK)
-	// {
-	// 	return text + MB2WCHAR("error sasl.mechanism");
-	// }
+	if (rd_kafka_conf_set(conf, "sasl.mechanism", "PLAIN",
+						  errstr, sizeof(errstr)) != RD_KAFKA_CONF_OK)
+	{
+		return text + MB2WCHAR("error sasl.mechanism");
+	}
 
-	// if (rd_kafka_conf_set(conf, "sasl.username", "svc.mdm",
-	// 					  errstr, sizeof(errstr)) != RD_KAFKA_CONF_OK)
-	// {
-	// 	return text + MB2WCHAR("error sasl.username");
-	// }
+	if (rd_kafka_conf_set(conf, "sasl.username", "svc.mdm",
+						  errstr, sizeof(errstr)) != RD_KAFKA_CONF_OK)
+	{
+		return text + MB2WCHAR("error sasl.username");
+	}
 
-	// if (rd_kafka_conf_set(conf, "sasl.password", "pahNg0uv0xohgh6k",
-	// 					  errstr, sizeof(errstr)) != RD_KAFKA_CONF_OK)
-	// {
-	// 	return text + MB2WCHAR("error sasl.password");
-	// }
+	if (rd_kafka_conf_set(conf, "sasl.password", "pahNg0uv0xohgh6k",
+						  errstr, sizeof(errstr)) != RD_KAFKA_CONF_OK)
+	{
+		return text + MB2WCHAR("error sasl.password");
+	}
 
-	// rk = rd_kafka_new(RD_KAFKA_PRODUCER, conf, errstr, sizeof(errstr));
-	// if (!rk)
-	// {
-	// 	fprintf(stderr, "%% Failed to create new producer: %s\n",
-	// 			errstr);
-	// 	return text + MB2WCHAR("error Failed to create new producer");
-	// }
+	rk = rd_kafka_new(RD_KAFKA_PRODUCER, conf, errstr, sizeof(errstr));
+	if (!rk)
+	{
+		return text + MB2WCHAR("error Failed to create new producer");
+	}
 
-	// rd_kafka_conf_set_dr_msg_cb(conf, dr_msg_cb);
+	rd_kafka_conf_set_dr_msg_cb(conf, dr_msg_cb);
 
-	// rk = rd_kafka_new(RD_KAFKA_PRODUCER, conf, errstr, sizeof(errstr));
-	// if (!rk)
-	// {
-	// 	fprintf(stderr, "%% Failed to create new producer: %s\n",
-	// 			errstr);
-	// 	return text + MB2WCHAR("error Failed to create new producer");
-	// }
+	signal(SIGINT, stop);
 
-	// signal(SIGINT, stop);
+	rd_kafka_resp_err_t err;
 
-	// rd_kafka_resp_err_t err;
+	string t = u16string_to_string(p_text1);
+	err = rd_kafka_producev(
+		rk,
+		RD_KAFKA_V_TOPIC(topic),
+		RD_KAFKA_V_MSGFLAGS(RD_KAFKA_MSG_F_COPY),
+		RD_KAFKA_V_KEY(const_cast<char *>(t.c_str()), t.size()),
+		RD_KAFKA_V_VALUE(const_cast<char *>(t.c_str()), t.size()),
+		RD_KAFKA_V_OPAQUE(NULL),
+		RD_KAFKA_V_END);
 
-	// string t = "fuck";
+	if (err)
+	{
+		/*
+		 * Failed to *enqueue* message for producing.
+		 */
+		if (err == RD_KAFKA_RESP_ERR__QUEUE_FULL)
+		{
+			rd_kafka_poll(rk,
+						  1000 /*block for max 1000ms*/);
+		}
+		return text + MB2WCHAR("Failed to produce to topic");
+	}
+	else
+	{
+		return text + MB2WCHAR("Persisted");
+	}
 
-	// err = rd_kafka_producev(
-	// 	rk,
-	// 	RD_KAFKA_V_TOPIC(topic),
-	// 	RD_KAFKA_V_MSGFLAGS(RD_KAFKA_MSG_F_COPY),
-	// 	RD_KAFKA_V_KEY(const_cast<char *>(t.c_str()), t.size()),
-	// 	RD_KAFKA_V_VALUE(const_cast<char *>(t.c_str()), t.size()),
-	// 	RD_KAFKA_V_OPAQUE(NULL),
-	// 	RD_KAFKA_V_END);
+	rd_kafka_poll(rk, 0 /*non-blocking*/);
+	rd_kafka_flush(rk, 1000 /* wait for max 10 seconds */);
+	rd_kafka_destroy(rk);
 
-	// if (err)
-	// {
-	// 	if (err == RD_KAFKA_RESP_ERR__QUEUE_FULL)
-	// 	{
-	// 		return text + MB2WCHAR("error Failed to produce to topic");
-	// 	}
-	// }
-	// else
-	// {
-		
-	// }
-
-	// rd_kafka_poll(rk, 500 /*non-blocking*/);
-
-	// rd_kafka_flush(rk, 1000 /* wait for max 10 seconds */);
-
-	// rd_kafka_destroy(rk);
-
-	return p_text1 + p_text2;
+	return text + MB2WCHAR("Persisted");
 }
 
 void TestComponent::setTestString(const std::u16string &text)
--- swd/version.h
+++ swd/version.h
@@ -1,6 +1,6 @@
 #define VER_FILENAME AddinNative
-#define VERSION_FULL 0.0.0.6
+#define VERSION_FULL 0.0.0.9
 #define VERSION_MAJOR     0
 #define VERSION_MINOR     0
 #define VERSION_REVISION  0
-#define VERSION_BUILD     1
+#define VERSION_BUILD     9
